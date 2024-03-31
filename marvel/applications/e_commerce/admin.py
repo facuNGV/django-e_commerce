95aac8fa4b7073e3afe26ce0867d564f79ed2877
facuNpc <facu_nicogas@outlook.com>
2024-03-31 01:55:34 -0300

    añade y registro de modelos

--- marvel/applications/e_commerce/admin.py
+++ marvel/applications/e_commerce/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from e_commerce.models import *
+
+
+@admin.register(Comic)
+class ComicsAdmin(admin.ModelAdmin):
+    list_display = ('marvel_id', 'title', 'stock_qty', 'price')
+
+    list_filter= ('marvel_id','title')
+
+    search_fields = ['title']
+
+    fieldsets = (
+        (None, {
+            'fields': ('marvel_id', 'title', 'stock_qty')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('description','price', 'picture'),
+        }),
+    )
+
+
+@admin.register(WishList)
+class WishListAdmin(admin.ModelAdmin):
+    list_display = ('user', 'comic', 'favorite', 'cart')
+    list_display_links = ('user', 'comic')
+    list_filter= ('favorite','cart')
--- marvel/applications/e_commerce/models.py
+++ marvel/applications/e_commerce/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+from django.contrib.auth.models import User
+
+
+class Comic(models.Model):
+
+    id = models.BigAutoField(db_column='ID', primary_key=True)
+    marvel_id = models.PositiveIntegerField(
+        verbose_name='marvel id', null=False, blank=False, unique=True
+    )
+    title = models.CharField(
+        verbose_name='title', max_length=120, default=''
+    )
+    description = models.TextField(verbose_name='description', default='')
+    price = models.FloatField(
+        verbose_name='price', max_length=5, default=0.00
+    )
+    stock_qty = models.PositiveIntegerField(
+        verbose_name='stock qty', default=0
+    )
+    picture = models.URLField(verbose_name='picture', default='')
+
+    class Meta:
+        db_table = 'e_commerce_comics'
+        verbose_name = 'comic'
+        verbose_name_plural = 'comics'
+
+    def __str__(self):
+        return f'{self.id} - {self.title}'
+
+
+class WishList(models.Model):
+    id = models.BigAutoField(db_column='ID', primary_key=True)
+    user = models.ForeignKey(
+        User,
+        verbose_name='user',
+        on_delete=models.CASCADE,
+        default=1,
+        blank=True
+    )
+    comic = models.ForeignKey(
+        Comic,
+        verbose_name='comic',
+        on_delete=models.CASCADE,
+        default=1,
+        blank=True
+    )
+    favorite = models.BooleanField(verbose_name='favorite', default=False)
+    cart = models.BooleanField(verbose_name='cart', default=False)
+    wished_qty = models.PositiveIntegerField(
+        verbose_name='wished qty', default=0
+    )
+    bought_qty = models.PositiveIntegerField(
+        verbose_name='bought qty', default=0
+    )
+
+    class Meta:
+        db_table = 'e_commerce_wish_list'
+        verbose_name = 'wish list'
+        verbose_name_plural = 'wish lists'
+
+    def __str__(self):
+        return f'{self.id}'
+
